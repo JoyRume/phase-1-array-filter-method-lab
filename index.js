9d13ace7f0b44c4501d2f1a4b2f426c9107ee456
JoyRume <joy.rume@student.moringaschool.com>
2023-12-24 17:05:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+
+function findMatching(drivers,names){
+ return drivers.filter(drivers=>drivers.toLowerCase()===names.toLowerCase())
+}
+
+
+//function fuzzyMatch(driver ,firstName){
+   // return driver.filter(driver=>driver.firstName.startWith(firstName))
+
+//}
+
+
+const fuzzyMatch = (driver, firstName) =>{
+    return driver.filter(driver => 
+    driver.indexOf(firstName) === 0);
+}
+
+function matchName(driver,name ,homeTown){
+    return driver.filter(driver=>driver.name===name && driver.homeTown===homeTown)
+
+}
